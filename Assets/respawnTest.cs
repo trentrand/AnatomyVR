65f367d049dc92f441ef2565d902931a394ee2e7
Avery Batson <avery@itsa.academy>
2016-07-29 16:49:57 -0700

    worked on blood sceen

---
+++ Assets/respawnTest.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class respawnTest : MonoBehaviour {
+Vector3 initialPosition;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(resetPositionOnInterval());
+        initialPosition = gameObject.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    IEnumerator resetPositionOnInterval()
+    {
+        yield return new WaitForSeconds(Random.Range(5, 10));
+
+        // Reset position, then wait another Random seconds
+        transform.position = initialPosition;
+        
+        StartCoroutine(resetPositionOnInterval());
+    }
+}
