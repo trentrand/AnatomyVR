b86ff7172920165f1cd9b4b2e4bee47de4842588
Geovanni Orozco <gxorozco02@gmail.com>
2016-07-29 18:43:04 -0700

    Added ghost placeholder effect

--- Assets/SnapInPlaceScript.cs
+++ Assets/SnapInPlaceScript.cs
@@ -2,30 +2,54 @@
 using System.Collections;
 using NewtonVR;
 
-public class SnapInPlaceScript : MonoBehaviour {
+public class SnapInPlaceScript : MonoBehaviour
+{
+    // Store initial transform values
     Vector3 initialPosition;
-    Quaternion iniRot;
-    public Rigidbody rb;
-    public NVRInteractableItem grabScript;
-    public bool grabbed = false;
+    Quaternion initialRotation;
+
+    // Reference the Rigidbody
+    Rigidbody rb;
+
+    // Reference the NVRInteractableItem script
+    NVRInteractableItem grabScript;
+
+    // Reference ghost placeholder GameObject
+    public GameObject ghostPlaceholder;
 
     // Use this for initialization
-    void Start () {
-	rb = GetComponent<Rigidbody>();
-    initialPosition = transform.position;
-    grabScript = GetComponent<NVRInteractableItem>();
-    iniRot = transform.rotation;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-        if (grabScript.AttachedHand == null && Vector3.Distance(initialPosition, transform.position) < 0.2)
+    void Start()
+    {
+        // Retrieve reference to components
+        rb = GetComponent<Rigidbody>();
+        grabScript = GetComponent<NVRInteractableItem>();
+
+        // Store initial transform values
+        initialPosition = transform.position;
+        initialRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If the <gameObject> is grabbed
+        if (grabScript.AttachedHand != null)
         {
+            if (ghostPlaceholder != null)
+                // Show placeholder ghost outline
+                ghostPlaceholder.SetActive(true);
+        }
+        // Else if <gameObject> is not grabbed and position is close to original position
+        else if (Vector3.Distance(initialPosition, transform.position) < 0.2)
+        {
+            if (ghostPlaceholder != null)
+                // Disable placeholder ghost outline
+                ghostPlaceholder.SetActive(false);
+
+            // Snap back to initial position
             rb.isKinematic = true;
             transform.position = initialPosition;
-            transform.rotation = iniRot;
+            transform.rotation = initialRotation;
         }
-
-	}
+    }
 }
