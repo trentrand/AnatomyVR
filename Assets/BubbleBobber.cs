bca4dae1ac3942b0fbc3603533c66c21f7c98e21
Avery Batson <avery@itsa.academy>
2016-08-01 16:47:09 -0700

    bubbles

---
+++ Assets/BubbleBobber.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BubbleBobber : MonoBehaviour {
+
+ private float timer = 0.0f;
+  float bobbingSpeed = 0.08f;
+  float bobbingAmount = 0.01f;
+  public float midpoint = 0.3f;
+  
+  void Update () {
+     float waveslice = 0.0f;
+  
+     Vector3 cSharpConversion = transform.localPosition; 
+  
+        waveslice = Mathf.Sin(timer);
+        timer = timer + bobbingSpeed;
+        if (timer > Mathf.PI * 2) {
+           timer = timer - (Mathf.PI * 2);
+        }
+
+     if (waveslice != 0) {
+        float translateChange = waveslice * bobbingAmount;
+        float totalAxes = Mathf.Clamp (1, 0.0f, 1.0f);
+        translateChange = totalAxes * translateChange;
+        cSharpConversion.y = midpoint + translateChange;
+     }
+     else {
+        cSharpConversion.y = midpoint;
+     }
+  
+       transform.localPosition = cSharpConversion;
+  }
+  
+  
+ 
+ }
---
+++ Assets/GrowBubble.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GrowBubble : MonoBehaviour {
+    public bool trueOrFalse = false;
+    float growNum = 0;
+    BubbleBobber floatHeight;
+    // Use this for initialization
+    void Start()
+    {
+
+        floatHeight = GetComponent<BubbleBobber>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trueOrFalse == true)
+        {
+            if (growNum <= 0.31)
+            {
+                transform.localScale = new Vector3(growNum, growNum, growNum);
+
+                growNum = growNum + 0.1f;
+
+                floatHeight.midpoint = floatHeight.midpoint + growNum/2;
+
+            }
+
+        }
+        else
+        {
+            transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            growNum = 0.1f;
+            floatHeight.midpoint = 0.3f;
+        }
+    }
+
+
+}
+
+
