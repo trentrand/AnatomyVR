b90d2159d6b4ffbf418a5d0295429ec949902bfb
Geovanni Orozco <gxorozco02@gmail.com>
2016-08-04 15:34:28 -0700

    orbs

--- Assets/InfoStation.cs
+++ Assets/InfoStation.cs
@@ -10,6 +10,9 @@ public class InfoStation : MonoBehaviour
     BoxCollider triggerCollider;
     public GrowBubble bubbleBig;
     public string broadCaster = "none";
+    GameObject image;
+    public Material blood2;
+    public Material none;
 
     void Start()
     {
@@ -17,6 +20,7 @@ public class InfoStation : MonoBehaviour
         factCollection = FactContainer.Load(xmlPath);
         triggerCollider = GetComponent<BoxCollider>();
         bubbleBig = GameObject.Find("Animation Orb").GetComponent<GrowBubble>();
+
 
     }
 
@@ -27,6 +31,9 @@ public class InfoStation : MonoBehaviour
             case Anatomy.heart:
                 Info.text = factCollection.facts[0].TextA;
                 bubbleBig.growBubble = true;
+
+                GameObject.Find("Animation Orb (1)").GetComponent<MeshRenderer>().material = blood2;
+
                 broadCaster = "heart";
                 break;
 
@@ -55,6 +62,8 @@ public class InfoStation : MonoBehaviour
             case Anatomy.heart:
                 Info.text = "";
                 bubbleBig.growBubble = false;
+                GameObject.Find("Animation Orb (1)").GetComponent<MeshRenderer>().material = none;
+
 
                 break;
             case Anatomy.lungs:
