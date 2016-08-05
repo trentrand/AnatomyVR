c1bf57d90b042cb945bff5c7ad63fbc0a8d1103c
Geovanni Orozco <gxorozco02@gmail.com>
2016-08-05 16:54:40 -0700

    labels

---
+++ Assets/faceCam.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class faceCam : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Face the camera directly.
+        transform.LookAt(Camera.main.transform.position);
+
+        // Rotate so the visible side faces the camera.
+        transform.Rotate(0, 180, 0);
+    }
+}
