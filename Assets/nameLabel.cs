95e4e39930e67bba1ce73f18030dcd3f5dd4b3a2
Geovanni Orozco <gxorozco02@gmail.com>
2016-08-08 10:58:55 -0700

    labels

--- Assets/nameLabel.cs
+++ Assets/nameLabel.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class nameLabel : MonoBehaviour {
+public class nameLabel : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+        Label.text = "banana";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
