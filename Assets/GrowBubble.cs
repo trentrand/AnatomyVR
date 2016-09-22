a3c4e3bb6f15b4f743f99d86e30e668c5d115aa1
Matt Lake <melakeca@gmail.com>
2016-09-22 15:45:19 -0700

    textFix+bubbleScriptFix

--- Assets/GrowBubble.cs
+++ Assets/GrowBubble.cs
@@ -32,7 +32,7 @@ public class GrowBubble : MonoBehaviour
         }
         else
         {
-            transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            transform.localScale = new Vector3(0.1242573f, 0.1242573f, 0.1242573f);
             growNum = 0.1f;
             floatHeight.midpoint = 0.5f;
         }
