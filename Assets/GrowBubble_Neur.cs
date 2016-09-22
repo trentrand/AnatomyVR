28538e367e8ba8df281251787d5dd3c5bee95320
Matt Lake <melakeca@gmail.com>
2016-09-22 16:24:24 -0700

    bubbleFixAllScenes

---
+++ Assets/GrowBubble_Neur.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GrowBubble_Neur : MonoBehaviour
+{
+    public bool growBubble = false;
+    float growNum = 0;
+    BubbleBobber floatHeight;
+    // Use this for initialization
+    void Start()
+    {
+
+        floatHeight = GetComponent<BubbleBobber>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (growBubble == true)
+        {
+            if (growNum <= 0.31)
+            {
+                transform.localScale = new Vector3(growNum, growNum, growNum);
+
+                growNum = growNum + 0.1f;
+
+                floatHeight.midpoint = floatHeight.midpoint + growNum / 2;
+
+            }
+
+        }
+        else
+        {
+            transform.localScale = new Vector3(0.1242573f, 0.1242573f, 0.1242573f);
+            growNum = 0.1f;
+            floatHeight.midpoint = 0.8f;
+        }
+    }
+}
