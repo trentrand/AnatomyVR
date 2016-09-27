9ff1cc8c6cf76cd69462f72452d244c9d8ba6cd0
Techcat42 <avery@itsa.academy>
2016-09-27 16:54:29 -0700

    introSceneAniamtionPaly

---
+++ Assets/LeverAnimationStart.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeverAnimationStart : MonoBehaviour {
+    public BoxCollider triggerCollider;
+    public Animator handMover;
+    // Use this for initialization
+    void Start () {
+        handMover = GameObject.Find("vr_controller_vive_1_5").GetComponent<Animator>();
+        handMover.applyRootMotion = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "LeverHandle")
+        {
+            handMover.applyRootMotion = true;
+
+        }
+    }
+}
