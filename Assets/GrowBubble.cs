95753e95f5fd4d8a6b0237de6af532f6bb22401b
Matt Lake <melakeca@gmail.com>
2016-09-27 16:05:45 -0700

    mainSceneBubble

--- Assets/GrowBubble.cs
+++ Assets/GrowBubble.cs
@@ -32,9 +32,9 @@ public class GrowBubble : MonoBehaviour
         }
         else
         {
-            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            transform.localScale = new Vector3(0.15f, 0.15f, 0.15f);
             growNum = 0.1f;
-            floatHeight.midpoint = 0.6f; // This tells where the bubble is floating
+            floatHeight.midpoint = 0.5f; // This tells where the bubble is floating
         }
     }
 }
