566ab18bf9c1770b334b99dc379a0b5a078796d6
Techcat42 <avery@itsa.academy>
2016-09-27 16:02:52 -0700

    lung movement 2

--- Assets/breathUpDownScript.cs
+++ Assets/breathUpDownScript.cs
@@ -6,7 +6,7 @@ public class breathUpDownScript : MonoBehaviour
 
     // Use this for initialization
     public int timer = 0;
-    bool down = false;
+    bool down = true;
     void Start()
     {
 
@@ -20,18 +20,18 @@ public class breathUpDownScript : MonoBehaviour
         {
             down = false;
         }
-        if (timer == 200)
+        if (timer == 160)
         {
             down = true;
         }
         if (down == false)
         {
-            transform.Translate(Vector3.up * Time.deltaTime * 65);
+            transform.Translate(Vector3.down * Time.deltaTime * 65);
             timer++;
         }
         if (down == true)
         {
-            transform.Translate(Vector3.down * Time.deltaTime * 65);
+            transform.Translate(Vector3.up * Time.deltaTime * 65);
             timer--;
         }
     }
