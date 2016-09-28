7906c1588a8055bdf057d7cfa98aca36abd4b046
Techcat42 <avery@itsa.academy>
2016-09-28 12:28:44 -0700

    intro scene transistion

---
+++ Assets/LeverToMainScene.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LeverToMainScene : MonoBehaviour
+{
+    public BoxCollider triggerCollider;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "LeverHandle")
+        {
+            SceneManager.LoadScene("Anatomy");
+        }
+    }
+}
